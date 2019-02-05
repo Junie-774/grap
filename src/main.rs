d41d5bb6a29f87de22adf4393559e91ecbc0fd47
Kyle Coblentz <chickengod37@gmail.com>
2019-02-04 23:20:13 -0500

    Add support for case insensitivity

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ fn main() {
 
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-    let ArgParseResult(ignore_case, invert_match, patterns, files) = match parse_args(args) {
+    let ArgParseResult(ignore_case, invert_match, mut patterns, files) = match parse_args(args) {
         Ok(result) => result,
         Err(_) => {
             println!("Usage: grap [OPTIONS]... PATTERN [FILES]...");
@@ -54,16 +54,30 @@ fn main() {
         }
     };
 
+    if ignore_case {
+        patterns = patterns.into_iter().map(|reg: Regex| {
+            Regex::new(reg.as_str()
+                .to_lowercase()
+                .as_str())
+                .unwrap()}
+        ).collect();
+    }
+
+    let patterns = patterns;
+
+
     // Used for filtering out non-matching lines
     let matches_pattern= |contents: &String| -> bool {
         let mut has_match = false;
 
+        let contents = if ignore_case {contents.to_lowercase()} else {contents.clone()};
         for pat in &patterns {
             has_match = has_match || pat.is_match(contents.as_str());
         }
 
         if invert_match { !has_match } else {has_match}
     };
+
 
     let mut lines: Vec<String> = Vec::new();
     for mut file in files {
